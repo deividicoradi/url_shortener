aec213b603e73fed5762d7d1e660f58480ba8cea
deividicoradi <deividi.coradii@gmail.com>
2024-02-26 20:46:35 -0300

    Add ShotCode decode with Base62

--- test/models/short_code_test.rb
+++ test/models/short_code_test.rb
@@ -24,4 +24,28 @@ class ShortCodeTest < ActiveSupport::TestCase
   test "Encode 999_999" do
     assert_equal "4c91", ShortCode.encode(999_999)
   end
+
+  test "Decode '0'" do
+    assert_equal 0, ShortCode.decode("0")
+  end
+
+  test "Decode '1'" do
+    assert_equal 1, ShortCode.decode("1")
+  end
+
+  test "Decode 'a'" do
+    assert_equal 10, ShortCode.decode("a")
+  end
+
+  test "Decode '10'" do
+    assert_equal 62, ShortCode.decode("10")
+  end
+
+  test "Decode 'gw'" do
+    assert_equal 1024, ShortCode.decode("gw")
+  end
+
+  test "Decode '4c91'" do
+    assert_equal 999_999, ShortCode.decode("4c91")
+  end
 end
